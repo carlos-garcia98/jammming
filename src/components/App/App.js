7255e527b87923ef5e677720748a4a0a42e0da05
Carlos Garcia <carlos0698@live.com>
2024-07-19 17:36:27 -0600

    Add a connect to spotify component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
 import styles from "./App.module.css";
+import ConnectToSpotify from "../ConnectToSpotify/ConnectToSpotify";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 
 function App() {
   return (
     <>
-      <header>
-        <h1>
+      <header className={styles.header}>
+        <h1 className={styles.headerTitle}>
           Ja<span>mmm</span>ing
         </h1>
+        <ConnectToSpotify />
       </header>
       <main>
-        <SearchBar className={styles.searchBar} />
+        <SearchBar />
         <SearchResults />
       </main>
     </>
