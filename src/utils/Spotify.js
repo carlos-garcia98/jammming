ca81ca5eff3d4886ddf895099575c9a752e363d0
Carlos Garcia <carlos0698@live.com>
2024-08-01 14:22:27 -0600

    Add logic for the Save to spotify button.

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -48,6 +48,45 @@ const Spotify = {
       uri: track.uri,
     }));
   },
+
+  async savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+
+    try {
+      const userResponse = await fetch("https://api.spotify.com/v1/me", {
+        headers: headers,
+      });
+      const userJson = await userResponse.json();
+      const userId = userJson.id;
+
+      const playlistResponse = await fetch(
+        `https://api.spotify.com/v1/users/${userId}/playlists`,
+        {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({ name: name }),
+        }
+      );
+      const playlistJson = await playlistResponse.json();
+      const playlistId = playlistJson.id;
+
+      await fetch(
+        `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+        {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({ uris: trackUris }),
+        }
+      );
+    } catch (error) {
+      console.error("Error saving playlist:", error);
+    }
+  },
 };
 
 export default Spotify;
